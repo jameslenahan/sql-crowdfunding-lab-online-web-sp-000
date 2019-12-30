d5178a73448b449e4640ae459ded78411666b853
James Lenahan <james.k.lenahan@gmail.com>
2019-12-30 17:11:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("3", "5", 200, "6/1/19", "6/30/19");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("adfg", "erg", 85, "6/1/19", "6/30/19");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("sdv", "waeg", 55, "6/1/19", "6/30/19");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Alzheimers Run", "Seniors", 100, "6/1/19", "6/30/19");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("dfbs", "gyjfghj" 150, "6/1/19", "6/30/19");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Counseling Sessions", "Domestic Violence", 65, "7/1/19", "7/31/19");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Wig Making", "Cancer Survivors", 1000, "7/1/19", "7/31/19");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Somewhere", "there", 500, "7/1/1", "7/31/19");
